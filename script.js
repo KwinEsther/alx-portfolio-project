cb5fdfe80163d8d32429e413c69d5bcffd84e3ce
KwinEsther <esthernkanta6@gmail.com>
2025-01-08 14:36:06 +0100

    chore: select DOM elements for form, input, and habit list

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select DOM elements
+const habitForm = document.getElementById('habit-form');
+const habitInput = document.getElementById('habit-input');
+const habitList = document.getElementById('habit-list');
+
+// Log elements to verify
+console.log(habitForm, habitInput, habitList);
