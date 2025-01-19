12e115c5e4dd65fb90b12d87f9343f33956252d9
Esther Nkanta <esthernkanta6@gmail.com>
2025-01-19 19:53:07 +0100

    Updated server.js
    Made edits to server.js
    - Now uses updated cors package
    - Added JWT to .env

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get('/', (req, res) => {
+    res.send('Habit Tracker Backend is Running!');
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
